7c4940181bf0170b2eb0d5876287d675dd4c6fc5
ali faisal <alifaisalas99@gmail.com>
2020-04-02 23:28:11 +0500

    cache-integrated

--- src/main.py
+++ src/main.py
@@ -2,10 +2,38 @@ import nltk
 import openpyxl
 from src.pre_processing import Preprocessing
 
+
 # ---------------------------------- main code ----------------------------------
 if __name__ == "__main__":
 
     nltk.data.path.append("../resource/nltk_data/")
+
+    cache = 0
+    try:
+        workbook = openpyxl.load_workbook("../out/tf-idf.xlsx")
+        cache = 1   # file is already created, no need to re-evaluate docs
+    except FileNotFoundError:
+        cache = 0   # file is not created, scores will be calculated
+
+    if cache == 0:
+        # create a file
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        # total columns to be created: 118
+        cell = sheet.cell(1, 1).value = "words"
+        for i in range(2, 58):
+            sheet.cell(1, i).value = "d.tf:" + str(i-2)
+        sheet.cell(1, 58).value = "q.tf"
+        sheet.cell(1, 59).value = "df"
+        sheet.cell(1, 60).value = "idf"
+        for i in range(61, 117):
+            sheet.cell(1, i).value = "d.val:" + str(i-61)
+        sheet.cell(1, 117).value = "q.val"
+        workbook.save("../out/tf-idf.xlsx")
+        workbook.close()
+    else:
+        print("cache ready")
+
 
     stop_file = open("../resource/stopword-list.txt", "r")
     stop_list = stop_file.read().split("\n")
@@ -29,4 +57,4 @@ if __name__ == "__main__":
             pass
     # </pre-processing>
 
-    # print(bag_of_words.keys())
+    len_of_bag_of_words = len(bag_of_words)
