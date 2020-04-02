dc2cd89cf4197b7006f253ae53a412c00ea0b2c1
ali faisal <alifaisalas99@gmail.com>
2020-04-02 22:05:42 +0500

    imported-openpyxl

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from src.pre_processing import Preprocessing
 import nltk
+import openpyxl
+from src.pre_processing import Preprocessing
 
 # ---------------------------------- main code ----------------------------------
 if __name__ == "__main__":
@@ -9,18 +10,23 @@ if __name__ == "__main__":
     stop_file = open("../resource/stopword-list.txt", "r")
     stop_list = stop_file.read().split("\n")
 
+    bag_of_words = {}
+
     file_name = "../resource/trump-speeches/speech_0.txt"
     file = open(file_name, "r")
-    file.readline()     # skips the title
+    file.readline()  # skips the title
 
     # <pre-processing>
     pre_processing = Preprocessing()
     pre_processing.stop_word = stop_list
     tokens = pre_processing.tokenizer(file.read())
     lemmas = pre_processing.lemmatizer(tokens)
+
+    for lem in lemmas:
+        try:
+            bag_of_words[lem] = 0
+        except KeyError:
+            pass
     # </pre-processing>
 
-
-
-
-
+    # print(bag_of_words.keys())
