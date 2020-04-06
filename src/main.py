73bd0b642e0d6da9e03ab6c13d4ec9df1a9a3c70
ali faisal <alifaisalas99@gmail.com>
2020-04-06 12:48:58 +0500

    cache-perf-improvement

--- src/main.py
+++ src/main.py
@@ -236,12 +236,21 @@ if __name__ == "__main__":
     # and uses the 'cache' technique to store the calculated doc vectors
     # and bag-of-words in a file named '../out/tf-idf.xlsx'
 
-    # when no cache is found, in one run the calculations take approx. 11 seconds of which
-    # 5 seconds is to pre-process the corpora for tokens, lemma, bag-of-words formation, and
-    # 6 seconds to calculate tf-idf score for doc vectors
+    # when no cache is found, the calculations take approx. 8 seconds of which
+    #   5 seconds is to pre-process the corpora for tokens, lemma, bag-of-words formation, and
+    #   3 seconds to calculate tf-idf score for doc vectors
 
-    # when cache is found, it only takes approx. 6 seconds or less to load the cache file into memory
-    # hence a performance improvement of 5 seconds is achieved with no extra CPU utilization
+    # when cache is found,
+
+    # SCENARIO A: ****** LOADING CACHE IN READ-WRITE MODE ******
+    #   it only takes approx. 5 seconds to load the cache file into memory
+    #   hence a performance improvement of 3 seconds is achieved with no extra CPU utilization
+
+    # SCENARIO B: ****** LOADING CACHE IN READ-ONLY MODE ******
+    #   it only takes approx. 1 seconds to load the cache file into memory
+    #   hence a performance improvement of 7+ seconds is achieved with no extra CPU utilization
+
+    # Therefore, Cache will be loaded in read-only mode for maximum performance
 
     # imports the data required for lemmatization
     import_nltk_data("../resource/nltk_data/")
@@ -253,7 +262,7 @@ if __name__ == "__main__":
     try:
         # if try successful, file is already created, no need to re-evaluate docs
         # load the workbook
-        workbook = openpyxl.load_workbook("../out/tf-idf.xlsx")
+        workbook = openpyxl.load_workbook("../out/tf-idf.xlsx", read_only=True)
         # load the sheet
         sheet = workbook["Sheet"]
         print(datetime.now().strftime("%H:%M:%S") + ": cache found and loaded")
