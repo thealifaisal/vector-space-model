8fc78a2b2168b1e542c86492fc0a6b0cc2512c0b
ali faisal <alifaisalas99@gmail.com>
2020-04-08 02:19:05 +0500

    updated-documentation

--- src/main.py
+++ src/main.py
@@ -9,26 +9,15 @@ if __name__ == "__main__":
     # object of vsm class created that has methods for document and query processing
     vsm = VSM()
 
-    # this search-engine is based on vector-space-model
-
-    # and uses the 'cache' technique to store the calculated doc vectors
+    # search engine uses the 'cache' technique to store the calculated doc vectors
     # and bag-of-words in a file named '../out/tf-idf.xlsx'
 
-    # when no cache is found, the calculations take approx. 8 seconds of which
-    #   5 seconds is to pre-process the corpora for tokens, lemma, bag-of-words formation, and
-    #   3 seconds to calculate tf-idf score for doc vectors
+    # when cache is NOT found,
+    #   the whole program with query executed in 18 seconds.
 
-    # when cache is found,
-
-    # SCENARIO A: ****** LOADING CACHE IN READ-WRITE MODE ******
-    #   it only takes approx. 5 seconds to load the cache file into memory
-    #   hence a performance improvement of 3 seconds is achieved with no extra CPU utilization
-
-    # SCENARIO B: ****** LOADING CACHE IN READ-ONLY MODE ******
-    #   it only takes approx. 1 seconds to load the cache file into memory
-    #   hence a performance improvement of 7+ seconds is achieved with no extra CPU utilization
-
-    # Therefore, Cache will be loaded in read-only mode for maximum performance
+    # when cache IS found,
+    #   the whole program with query executed in 10 seconds.
+    # hence, a performance improvement of ** 8 ** seconds is achieved with no extra CPU utilization
 
     # imports the data required for lemmatization
     vsm.import_nltk_data("../resource/nltk_data/")
@@ -62,10 +51,10 @@ if __name__ == "__main__":
         print(datetime.now().strftime("%H:%M:%S") + ": cache saved to disk")
 
     # takes query from user as string appends a period to the end of query for tokenizer handling
-    query = input(datetime.now().strftime("%H:%M:%S") + "search: ") + "."
-    # query = "muslim "
-    alpha = float(input("enter alpha: "))
-    # alpha = 0.0005
+    # query = input(datetime.now().strftime("%H:%M:%S") + "search: ") + "."
+    query = "biggest plane wanted hour "
+    # alpha = float(input("enter alpha: "))
+    alpha = 0.0005
 
     vsm.update_doc_sheet(doc_sheet, query)
     result_set = vsm.create_result_set(doc_sheet, alpha)
